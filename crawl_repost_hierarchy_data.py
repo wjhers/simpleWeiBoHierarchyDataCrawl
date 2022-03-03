16da78040ca27c78a9ac966a509638a5ce2e8973
wjhers <wujh2117@mails.jlu.edu.cn>
2022-03-03 14:06:45 +0800

    fixed bugs

--- crawl_repost_hierarchy_data.py
+++ crawl_repost_hierarchy_data.py
@@ -324,7 +324,7 @@ if not os.path.exists(outputFolder):
 
 # 先获取热点链接
 saveFilePath = './hotlinks/link.csv'
-# get_repost_1(limit_pages=10,filepath=saveFilePath)
+get_repost_1(limit_pages=10,filepath=saveFilePath)
 # 读取热点链接
 df = pd.read_csv(saveFilePath)
 weibo_links = df['hotlink'].values.tolist()
